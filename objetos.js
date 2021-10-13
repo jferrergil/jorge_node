c58ba6788399dcf789c73b559fc47e5e7e64d2b5
jferrergil <jferrergil1@gmail.com>
2021-10-13 17:02:59 +0200

    mas apuntes mas y mas

---
+++ objetos.js
@@ -0,0 +1,9 @@
+const sherlock = {
+  surname: 'Holmes',
+  age: 64,
+};
+
+console.log(sherlock.job?.boss);
+
+// los bjetos se componnen de propiedades, estas propiedades funiconan como varibles,
+// las propiedades se asignan

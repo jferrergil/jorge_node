082a19517388944c7ce153bdc57ccecd46aff4bd
jferrergil <jferrergil1@gmail.com>
2021-10-13 10:07:59 +0200

    mas apuntes dia 13_10

---
+++ casting.js
@@ -0,0 +1,13 @@
+const a = 2;
+const b = '3';
+
+console.log(a + b);
+console.log(a - b);
+console.log(a - 'pepe');
+
+function suma(a, b) {
+  const result = a + b;
+  return result;
+}
+
+console.log(suma());

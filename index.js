5aa3c5ebf333b5ead2226696341be0aa259fc81e
jferrergil <jferrergil1@gmail.com>
2021-10-14 16:46:22 +0200

    mas aountes en index

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
-/* eslint-disable import/no-self-import */
-import { saludo } from './helpers.js';
+function algo(a) {
+  a = null;
+}
 
-const persona = 'Pepe';
-saludo(persona);
+function otra(a) {
+  const b = { ...a }; //Desetructuracion de un objet, es parecido al import de objetos
+
+  b.name = null;
+}
+
+let x = 2;
+let z = {};
+z.name = 'Pepe';
+
+algo(x);
+console.log({ x });
+otra(z);
+console.log({ z });
+
+// const aDatos = [1, 3, 6];
+// const aDatos2 = New Array ();
+// Array.isArray(aDatos);
+// console.log(Array.isArray(aDatos));
+
+let myName = 'Pepe';
+
+myName.normalize();
+
+console.log(aDatos.length);
+aDatos.push(2);
+console.log(Array.isArray(aDatos));
+
+// METODO MUTABLES
+
+aDatos.push(2);
+aDatos.sort();
+
+//METODOS INMUTABLES - usa callbacks
+
+// .forEach
+aDatos.forEach((i) => console.log(i));
+
+// .map
+const aDatos2 = aDatos.map((item) => item * 2);
+
+// metodo .filter
+const aDatosPares = aDatos.filter((i) => !(i % 2));
+
+// Arroe function
+const f = (w) => {
+  return w;
+};
+
+//arrow function
+const f = (w) => w;
